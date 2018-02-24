0eb8b17b0dfd7af30a252bd33e64db3ce86efd19
Rallade <rallade@hotmail.com>
2018-02-24 19:33:43 +0000

    new tuple

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from anvil_API_key import *
 
 anvil.server.connect(anvil_key)
 
-# {'dog': ('NOUN', '0.2', 128)} POS, Information_content, Hue
+# {'dog': ('NOUN', '0.2', 128, 2)} POS, Information_content, Hue, position_in_sentence
 
 @anvil.server.callable
+def get_rectangles(words):
+    for word in words:
+        words[word]
