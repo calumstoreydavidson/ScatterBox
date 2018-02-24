142f1c433e52aa26499c3d17c89c42d1007ce863
Rallade <rallade@hotmail.com>
2018-02-24 22:14:16 +0000

    First attempt at random rectangle generation

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 import anvil.server
 from anvil_API_key import *
+from random import *
+import math
+
 
 anvil.server.connect(anvil_key)
+anvil.server.wait_forever()
 
 # {'dog': ('NOUN', '0.2', 128, 2)} POS, Information_content, Hue, position_in_sentence
 
 @anvil.server.callable
 def get_rectangles(words):
+    i = 0
+    rects = []
     for word in words:
-        words[word]
+        width, height = words[word][1]
+        rect = {
+            x : i/len(words) * random(),
+            y : random(),
+            width : width,
+            height : height
+        }
+        rects.append(rect)
+
+def get_rectangle_dimensions(IC):
+    area = 10 * math.log(IC)
+    side = math.sqrt(area)
+    rectangleness = randint(-(int(side/2)), int(side/2))
+    side1 = side - rectangleness
+    side2 = area/side1
+    return side1, side2
